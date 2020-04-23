9a2284af0e65017c67bc44238672883bb5dd28d7
Gy <406631892@qq.com>
2020-04-23 10:26:37 +0800

    create website

---
+++ 0.79c37875f50b8a0091dd.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./src/lazy.js":
+/*!*********************!*\
+  !*** ./src/lazy.js ***!
+  \*********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return lazy; });\nfunction lazy() {\n    console.log('我是懒汉');\n}\n\n//# sourceURL=webpack:///./src/lazy.js?");
+
+/***/ })
+
+}]);
